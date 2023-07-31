68a456f6754a0bc8325e55e407660bdb15383f68
aaronmsimon <81662612+aaronmsimon@users.noreply.github.com>
2023-07-30 20:58:53 -0700

    Calculation Updates - adding drag
    Which helps to achieve terminal velocity

--- Ski Racer/Assets/Scripts/Physics.cs
+++ Ski Racer/Assets/Scripts/Physics.cs
@@ -4,28 +4,40 @@ public class Physics : MonoBehaviour
 {
     [SerializeField] private FloatVariable velocity;
 
+    [SerializeField] private FloatVariable mass;
     [SerializeField] private float gravity = -9.81f;
     [SerializeField] private float mountainSlope = 35f;
-
-    private float accX;
+    // [SerializeField] private float dragCoefficient = 1.05f;
+    [SerializeField] private float mu = .05f;
 
     private void Start() {
         velocity.Value = 0;
     }
 
-    private void Update() {
-        CalculateAccX();
-        float distance = KinematicDisplacement(velocity.Value, Time.deltaTime, accX);
+    private void FixedUpdate() {
+        float accX = CalculateAccX();
+        float drag = CalculateDrag(velocity.Value);
+        float acc = accX + drag;
 
+        float distance = KinematicDisplacement(velocity.Value, Time.deltaTime, acc);
         transform.Translate(new Vector3(0f, distance, 0f));
 
-        float velF = KinematicVelocity(velocity.Value, accX, Time.deltaTime);
+        float velF = KinematicVelocity(velocity.Value, acc, Time.deltaTime);
         velocity.Value = velF;
     }
 
-    private void CalculateAccX() {
+    private float CalculateAccX() {
         float rad = mountainSlope * Mathf.Deg2Rad;
-        accX = gravity * Mathf.Sin(rad);
+        float accX = mass.Value * gravity * (Mathf.Sin(rad) - mu * Mathf.Cos(rad));
+        return accX;
+    }
+
+    private float CalculateDrag(float velocity) {
+        float C = .2f;
+        float ro = 1.225f;
+        float A = 1f;
+        float drag = .5f * C * ro * A * velocity * velocity;
+        return drag;
     }
 
     private float KinematicDisplacement(float u, float t, float a) {
